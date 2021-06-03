d5826391ae5084bb8d1ce00c0f749bf76fa49750
msn-05 <saadmannuheen2005@gmail.com>
2021-06-03 13:10:16 +0600

    done

--- src/App.js
+++ src/App.js
@@ -9,6 +9,30 @@ export default function App() {
         <Input id='fname' name='fname' label='First name' type='text' />
         <Input id='lname' name='lname' label='Last name' type='text' />
       </Row>
+      <Row>
+        <Input id='email' name='email' label='E-mail' type='email' />
+        <Input id='subject' name='subject' label='Subject' type='text' />
+      </Row>
+      <Message id='msg' name='msg' label='Message' />
+      <br />
+      <div className='row' style={{width:'90%',margin:'auto',justifyContent:'flex-start'}}>
+        <div className='social'>
+          <h2>OR HERE</h2><br />
+          <h3>Email:</h3><br />
+          <div>example@mail.com</div><br />
+          <div>example@mail.com</div><br />
+          <div>example@mail.com</div><br />
+        </div>
+        <div className='social'>
+          <h2>Get Social</h2>
+          <a href='#' className='fa fa-twitter'></a>
+          <a href='#' className='fa fa-instagram'></a>
+          <a href='#' className='fa fa-linkedin'></a>
+        </div>
+        <div className='social'>
+          <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook&tabs=timeline&width=302&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="302" height="500" style={{"border":"none","overflow":"hidden"}} scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+        </div>
+      </div>
     </div>
   );
 }
@@ -31,13 +55,22 @@ function Row(props){
   );
 }
 function Input(props){
-  const inputStyle = {
-    'width': '70%'
-  }
   return (
     <div className='col'>
       <label for={props.id}>{props.label.toUpperCase()}:</label>
-      <input style={inputStyle} type={props.type} id={props.id} name={props.name} />
+      <input className='inp' type={props.type} id={props.id} name={props.name} />
+    </div>
+  );
+}
+function Message(props){
+  const style = {
+    'margin': '40px auto',
+    'width': '90%'
+  };
+  return (
+    <div style={style}>
+      <label for={props.id}>{props.label.toUpperCase()}:</label>
+      <textarea className='inp msg' id={props.id} name={props.name} rows='3' />
     </div>
   );
 }
